38f01c2167f73df4382ba53220fee2bf496c679c
larissamunhoz <larissa.munhoz.03@gmail.com>
2023-07-12 22:38:09 -0300

    projeto iniciado

---
+++ src/main/java/br/com/udemy/cursoTesteUnitario/CursoTesteUnitarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.cursoTesteUnitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTesteUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTesteUnitarioApplication.class, args);
+	}
+
+}
